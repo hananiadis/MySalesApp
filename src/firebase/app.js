4e0ed82e85ecc35e1351f182cb4e7a8be9c90f31
hananiadis <hananiadis@gmail.com>
2026-03-04 09:24:18 +0200

    Remove Firebase config from app.js
    Removed sensitive Firebase configuration details.

--- src/firebase/app.js
+++ src/firebase/app.js
@@ -4,12 +4,7 @@ import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAADPmQCg2916WMmKl1-H78bLQdLcLmtLY',
-  authDomain: 'mysalesapp-38ccf.firebaseapp.com',
-  projectId: 'mysalesapp-38ccf',
-  storageBucket: 'mysalesapp-38ccf.firebasestorage.app',
-  messagingSenderId: '570551628901',
-  appId: '1:570551628901:android:b03de7bb211e90f319028c',
+
 };
 
 let appInstance;
